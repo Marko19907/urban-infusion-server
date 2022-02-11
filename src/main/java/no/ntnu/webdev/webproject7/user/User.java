64314bf2e7270e72ef42774dd0d3899762de1093
Marko19907 <marko.stancic23@gmail.com>
2022-02-11 11:52:49 +0100

    Added the User class

--- src/main/java/no/ntnu/webdev/webproject7/user/User.java
+++ src/main/java/no/ntnu/webdev/webproject7/user/User.java
@@ -1,4 +1,48 @@
 package no.ntnu.webdev.webproject7.user;
 
 public class User {
+
+    private String id;
+    private boolean admin;
+    private String email;
+    private String password;
+
+    public User(String id, boolean admin, String email, String password) {
+        this.id = id;
+        this.admin = admin;
+        this.email = email;
+        this.password = password;
+    }
+
+    private String getId() {
+        return this.id;
+    }
+
+    private void setId(String id) {
+        this.id = id;
+    }
+
+    private boolean isAdmin() {
+        return this.admin;
+    }
+
+    private void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    private String getEmail() {
+        return this.email;
+    }
+
+    private void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String getPassword() {
+        return this.password;
+    }
+
+    private void setPassword(String password) {
+        this.password = password;
+    }
 }
