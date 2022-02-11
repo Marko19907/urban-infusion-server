a42ee5952e68bbfea6fa60a173c3e0327c39a1e0
Marko19907 <marko.stancic23@gmail.com>
2022-02-11 10:49:51 +0100

    Created a new Spring Boot project

---
+++ src/main/java/no/ntnu/webdev/webproject7/Webproject7Application.java
@@ -0,0 +1,13 @@
+package no.ntnu.webdev.webproject7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webproject7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webproject7Application.class, args);
+	}
+
+}
