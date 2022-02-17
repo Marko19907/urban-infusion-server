af145051b2720d2e2ae4ae299895e8a6482e81a9
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-02-17 11:25:28 +0100

    add product repository

---
+++ src/main/java/no/ntnu/webdev/webproject7/product/ProductRepository.java
@@ -0,0 +1,6 @@
+package no.ntnu.webdev.webproject7.product;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, String> {
+}
