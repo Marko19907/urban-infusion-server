835ccc94a4d7cefe1588a29b5cc0ec914e53340b
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-02-25 21:43:38 +0100

    make id type not be nullable

--- src/main/java/no/ntnu/webdev/webproject7/product/ProductRepository.kt
+++ src/main/java/no/ntnu/webdev/webproject7/product/ProductRepository.kt
@@ -4,4 +4,4 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ProductRepository : CrudRepository<Product, String?>
+interface ProductRepository : CrudRepository<Product, String>
