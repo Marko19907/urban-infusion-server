2a80c5ddc4cb7e5a166729631e01d7ce5781f3eb
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-02-26 00:53:18 +0100

    code cleanup

--- src/main/java/no/ntnu/webdev/webproject7/utilities/Utilities.kt
+++ src/main/java/no/ntnu/webdev/webproject7/utilities/Utilities.kt
@@ -1,12 +1,8 @@
 package no.ntnu.webdev.webproject7.utilities
 
-fun <T> iterableToList(iterable : Iterable<T>): List<T> {
-    return iterable.toList();
-}
-
 /**
  * Returns true if at least one of the given elements is null
  */
 fun <T> objectsNotNull(vararg input: T): Boolean {
-    return !input.any { e -> e == null }
+    return input.all { e -> e != null }
 }
