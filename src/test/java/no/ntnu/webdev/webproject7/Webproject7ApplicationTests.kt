55b1f20d5d2fbc98804bb5a326a58c6384274911
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-02-28 12:17:34 +0100

    convert test to Kotlin

---
+++ src/test/java/no/ntnu/webdev/webproject7/Webproject7ApplicationTests.kt
@@ -0,0 +1,11 @@
+package no.ntnu.webdev.webproject7
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+internal class Webproject7ApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
