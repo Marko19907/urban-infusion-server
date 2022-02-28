80690b5329530d48a7f7b6f227e8d7ef17355bf7
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-02-28 12:39:14 +0100

    setup a test

--- src/test/java/no/ntnu/webdev/webproject7/Webproject7ApplicationTests.kt
+++ src/test/java/no/ntnu/webdev/webproject7/Webproject7ApplicationTests.kt
@@ -1,11 +1,33 @@
 package no.ntnu.webdev.webproject7
 
+import no.ntnu.webdev.webproject7.comment.CommentRepository
 import org.junit.jupiter.api.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import kotlin.test.assertEquals
 
 @SpringBootTest
-internal class Webproject7ApplicationTests {
+@ActiveProfiles("test")
+internal class Webproject7ApplicationTests(
+    @Autowired
+    private val commentRepository: CommentRepository
+) {
+    @Value("\${spring.datasource.url}")
+    private lateinit var dataSource: String;
+
     @Test
     fun contextLoads() {
+        val logger: Logger = LoggerFactory.getLogger(this.javaClass);
+        logger.info(dataSource);
+    }
+
+    @Test
+    fun dbCrudTests() {
+        assertEquals(commentRepository.count(), 3);
+
     }
 }
