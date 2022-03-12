4eb8db5ca6a4da2252af57381e9568cbfc884554
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-03-12 16:08:23 +0100

    add CORS config for localhost:3000

---
+++ src/main/java/no/ntnu/webdev/webproject7/CorsConfiguration.kt
@@ -0,0 +1,15 @@
+package no.ntnu.webdev.webproject7
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+open class CorsConfiguration : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+    }
+}
