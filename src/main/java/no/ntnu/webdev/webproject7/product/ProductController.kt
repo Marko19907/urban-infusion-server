b17cb82231651c428128dc06247d79bf9e5dd810
Marko19907 <marko.stancic23@gmail.com>
2022-04-02 15:46:46 +0200

    Moved the categories to the product controller

--- src/main/java/no/ntnu/webdev/webproject7/product/ProductController.kt
+++ src/main/java/no/ntnu/webdev/webproject7/product/ProductController.kt
@@ -1,9 +1,22 @@
 package no.ntnu.webdev.webproject7.product
 
 import no.ntnu.webdev.webproject7.crud.CrudController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("products")
-class ProductController(productService: ProductService) : CrudController<Product, ProductId>(productService)
+class ProductController(productService: ProductService) : CrudController<Product, ProductId>(productService) {
+
+    @GetMapping("/categories")
+    fun getCategories(): ResponseEntity<MutableSet<Category>> {
+        return ResponseEntity(getCategoryMap(), HttpStatus.OK);
+    }
+
+    private fun getCategoryMap(): MutableSet<Category> {
+        return Category.values().toHashSet();
+    }
+}
