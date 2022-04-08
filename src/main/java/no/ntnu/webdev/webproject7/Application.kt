183b58c240a195ed1fb048c49dbfde439280a5a0
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-04-08 11:26:52 +0200

    refactor name of main application file

--- src/main/java/no/ntnu/webdev/webproject7/Application.kt
+++ src/main/java/no/ntnu/webdev/webproject7/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-open class Webproject7Application
+open class Application
 
 fun main(args: Array<String>) {
-    runApplication<Webproject7Application>(*args)
+    runApplication<Application>(*args)
 }
