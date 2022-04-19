ad76aeff86a77d623590848a626cf4aef94a9739
Marko19907 <marko.stancic23@gmail.com>
2022-04-19 19:12:41 +0200

    Created the registration controller

---
+++ src/main/java/no/ntnu/webdev/webproject7/controllers/RegistrationController.kt
@@ -0,0 +1,20 @@
+package no.ntnu.webdev.webproject7.controllers
+
+import no.ntnu.webdev.webproject7.dto.RegistrationDTO
+import no.ntnu.webdev.webproject7.services.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("register")
+class RegistrationController(private val userService: UserService) {
+
+    @PostMapping("")
+    fun registerNewUser(@RequestBody registrationDTO: RegistrationDTO): ResponseEntity<String> {
+        return if (userService.createUser(registrationDTO)) ResponseEntity(HttpStatus.OK) else ResponseEntity(HttpStatus.BAD_REQUEST)
+    }
+}
