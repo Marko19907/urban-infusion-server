15c30d35fad19cb2bd9c494ee1d1a4f4e2153005
Marko19907 <marko.stancic23@gmail.com>
2022-04-19 19:13:03 +0200

    Added the createUser() function to the UserService

--- src/main/java/no/ntnu/webdev/webproject7/services/UserService.kt
+++ src/main/java/no/ntnu/webdev/webproject7/services/UserService.kt
@@ -1,10 +1,57 @@
 package no.ntnu.webdev.webproject7.services
 
+import no.ntnu.webdev.webproject7.dto.RegistrationDTO
+import no.ntnu.webdev.webproject7.models.Role
 import no.ntnu.webdev.webproject7.models.User
 import no.ntnu.webdev.webproject7.models.UserEntityId
 import no.ntnu.webdev.webproject7.repositories.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 
+private const val MIN_PASSWORD_LENGTH = 8;
+private const val MAX_PASSWORD_LENGTH = 20;
+
 @Service
-class UserService(@Autowired userRepository: UserRepository) : CrudService<User, UserEntityId>(userRepository)
+class UserService(@Autowired private val userRepository: UserRepository) :
+    CrudService<User, UserEntityId>(userRepository) {
+
+    fun createUser(registrationDTO: RegistrationDTO): Boolean {
+        if (!registrationDTO.validate()) {
+            return false;
+        }
+        // TODO: Check if the username exists?
+        if (!checkPasswordLength(registrationDTO.password)) {
+            return false;
+        }
+        val user = createUserEntity(registrationDTO);
+        return add(user);
+    }
+
+    private fun createUserEntity(registrationDTO: RegistrationDTO): User {
+        return User(
+            registrationDTO.email,
+            registrationDTO.username,
+            encodePassword(registrationDTO.password),
+            "",
+            "",
+            "",
+            "",
+            Role.USER
+        );
+    }
+
+    private fun checkPasswordLength(password: String): Boolean {
+        if (password.isBlank()) {
+            return false;
+        }
+        if (password.length < MIN_PASSWORD_LENGTH || password.length > MAX_PASSWORD_LENGTH) {
+            return false;
+        }
+        return true;
+    }
+
+    private fun encodePassword(password: String): String {
+        return BCryptPasswordEncoder().encode(password);
+    }
+}
