1a673362d7858c90d62f961f91f8f4301ff4424f
Marko19907 <marko.stancic23@gmail.com>
2022-04-28 00:32:57 +0200

    Overridden the add function of the CommentService

--- src/main/java/no/ntnu/webdev/webproject7/services/CommentService.kt
+++ src/main/java/no/ntnu/webdev/webproject7/services/CommentService.kt
@@ -1,10 +1,28 @@
 package no.ntnu.webdev.webproject7.services
 
+import no.ntnu.webdev.webproject7.dto.CommentDTO
 import no.ntnu.webdev.webproject7.models.Comment
 import no.ntnu.webdev.webproject7.models.CommentId
+import no.ntnu.webdev.webproject7.models.User
 import no.ntnu.webdev.webproject7.repositories.CommentRepository
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class CommentService(@Autowired commentRepository: CommentRepository) : CrudService<Comment, CommentId>(commentRepository)
+class CommentService(private val commentRepository: CommentRepository) : CrudService<Comment, CommentId>(commentRepository) {
+
+    @Override
+    fun add(commentDTO: CommentDTO?, user: User?): Boolean {
+        if (commentDTO == null || user == null) {
+            return false;
+        }
+        if (!commentDTO.validate() || !user.validate()) {
+            return false;
+        }
+        val comment = this.createComment(commentDTO, user);
+        return this.commentRepository.save(comment).validate();
+    }
+
+    private fun createComment(commentDTO: CommentDTO, user: User): Comment {
+        return Comment(user, commentDTO.text, null);
+    }
+}
