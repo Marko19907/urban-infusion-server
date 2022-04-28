4b83bc2aa735871dc281157df9f440b03c69379d
Marko19907 <marko.stancic23@gmail.com>
2022-04-29 00:10:09 +0200

    Added a productID to the CommentDTO

--- src/main/java/no/ntnu/webdev/webproject7/dto/CommentDTO.kt
+++ src/main/java/no/ntnu/webdev/webproject7/dto/CommentDTO.kt
@@ -1,12 +1,14 @@
 package no.ntnu.webdev.webproject7.dto
 
+import no.ntnu.webdev.webproject7.models.ProductId
 import no.ntnu.webdev.webproject7.utilities.objectsNotNull
 
 data class CommentDTO(
+    val productID: ProductId,
     val text: String
 ) : DTO {
 
     override fun validate(): Boolean {
-        return objectsNotNull(this.text) && this.text.length <= 1000;
+        return objectsNotNull(this.text, this.productID) && this.text.length <= 1000;
     }
 }
