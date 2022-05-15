3ed2b6b37c66fe89f78c7de0b2283e2bb16fdef9
Marko19907 <marko.stancic23@gmail.com>
2022-05-16 00:15:06 +0200

    Improved the UserController

--- src/main/java/no/ntnu/webdev/webproject7/controllers/UserController.kt
+++ src/main/java/no/ntnu/webdev/webproject7/controllers/UserController.kt
@@ -3,9 +3,29 @@ package no.ntnu.webdev.webproject7.controllers
 import no.ntnu.webdev.webproject7.models.User
 import no.ntnu.webdev.webproject7.models.UserEntityId
 import no.ntnu.webdev.webproject7.services.UserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("users")
-class UserController(userService: UserService) : CrudController<User, UserEntityId>(userService)
+class UserController(@Autowired private val userService: UserService) {
+
+    @GetMapping("")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    fun all(): ResponseEntity<List<User>> {
+        return ResponseEntity(this.userService.all(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    fun getOne(@PathVariable id: UserEntityId): ResponseEntity<User> {
+        val entity = this.userService.getById(id);
+        return if (entity != null) ResponseEntity(entity, HttpStatus.OK) else ResponseEntity(HttpStatus.BAD_REQUEST);
+    }
+}
