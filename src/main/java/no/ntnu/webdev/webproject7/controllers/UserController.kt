28ea2efa3984e1d2a279263cdd939cf9dd9f57ef
Marko19907 <marko.stancic23@gmail.com>
2022-05-20 19:59:53 +0200

    Added patch and put endpoints that correspond to the nullable and non-nullable DTOs respectively

--- src/main/java/no/ntnu/webdev/webproject7/controllers/UserController.kt
+++ src/main/java/no/ntnu/webdev/webproject7/controllers/UserController.kt
@@ -2,6 +2,7 @@ package no.ntnu.webdev.webproject7.controllers
 
 import no.ntnu.webdev.webproject7.dto.UserPasswordUpdateDTO
 import no.ntnu.webdev.webproject7.dto.UserUpdateDTO
+import no.ntnu.webdev.webproject7.dto.UserUpdatePartialDTO
 import no.ntnu.webdev.webproject7.exceptions.UserUpdateFailedException
 import no.ntnu.webdev.webproject7.models.User
 import no.ntnu.webdev.webproject7.models.UserEntityId
@@ -13,6 +14,7 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -54,17 +56,16 @@ class UserController(@Autowired private val userService: UserService) {
         return ResponseEntity(HttpStatus.OK);
     }
 
-    @PatchMapping("")
+    @PutMapping("")
     @PreAuthorize("hasAuthority('USER')")
     fun replace(@RequestBody userUpdateDTO: UserUpdateDTO): ResponseEntity<String> {
-        val user: User = this.userService.getSessionUser() ?: return ResponseEntity("Invalid authentication!", HttpStatus.BAD_REQUEST);
-        try {
-            this.userService.update(userUpdateDTO, user);
-        }
-        catch (e: UserUpdateFailedException) {
-            return ResponseEntity(e.message, HttpStatus.BAD_REQUEST);
-        }
-        return ResponseEntity(HttpStatus.OK);
+        return this.updateReplaceUser(userUpdateDTO);
+    }
+
+    @PatchMapping("")
+    @PreAuthorize("hasAuthority('USER')")
+    fun update(@RequestBody userUpdatePartialDTO: UserUpdatePartialDTO): ResponseEntity<String> {
+        return this.updateReplaceUser(userUpdatePartialDTO);
     }
 
     @GetMapping("/me")
@@ -72,5 +73,18 @@ class UserController(@Autowired private val userService: UserService) {
     fun getUser(): ResponseEntity<User> {
         val user = this.userService.getSessionUser();
         return if (user != null) ResponseEntity(user, HttpStatus.OK) else ResponseEntity(HttpStatus.BAD_REQUEST);
+    }
+
+    private fun updateReplaceUser(userUpdatePartialDTO: UserUpdatePartialDTO): ResponseEntity<String> {
+        val user: User = this.userService.getSessionUser() ?: return ResponseEntity(
+            "Invalid authentication!",
+            HttpStatus.BAD_REQUEST
+        );
+        try {
+            this.userService.update(userUpdatePartialDTO, user);
+        } catch (e: UserUpdateFailedException) {
+            return ResponseEntity(e.message, HttpStatus.BAD_REQUEST);
+        }
+        return ResponseEntity(HttpStatus.OK);
     }
 }
