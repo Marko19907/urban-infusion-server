e54bc03e0ca874b870eabd564b6b548237c156dd
Marko19907 <marko.stancic23@gmail.com>
2022-05-24 00:56:51 +0200

    Refactored the FileUtilities

--- src/main/java/no/ntnu/webdev/webproject7/utilities/FileUtilities.kt
+++ src/main/java/no/ntnu/webdev/webproject7/utilities/FileUtilities.kt
@@ -5,17 +5,17 @@ class FileUtilities {
 }
 
 fun loadProductImage(imageName: String?, extension: String?): ByteArray? {
-    if (imageName == null || extension == null) {
-        return null;
-    }
-    val inputStream = FileUtilities::class.java.classLoader.getResourceAsStream("product-images/$imageName.$extension");
-    return inputStream?.readAllBytes();
+    return loadImage("product-images", imageName, extension);
 }
 
 fun loadUserImage(imageName: String?, extension: String?): ByteArray? {
+    return loadImage("user-images", imageName, extension);
+}
+
+private fun loadImage(path: String, imageName: String?, extension: String?): ByteArray? {
     if (imageName == null || extension == null) {
         return null;
     }
-    val inputStream = FileUtilities::class.java.classLoader.getResourceAsStream("user-images/$imageName.$extension");
+    val inputStream = FileUtilities::class.java.classLoader.getResourceAsStream("$path/$imageName.$extension");
     return inputStream?.readAllBytes();
 }
