19220759924672bbf0fc8b28d1b62ecb29d065aa
Marko19907 <marko.stancic23@gmail.com>
2022-06-05 19:19:19 +0200

    Changed a put to patch

--- src/main/java/no/ntnu/webdev/webproject7/controllers/CommentController.kt
+++ src/main/java/no/ntnu/webdev/webproject7/controllers/CommentController.kt
@@ -14,9 +14,9 @@ import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -57,7 +57,7 @@ class CommentController(
         }
     }
 
-    @PutMapping("")
+    @PatchMapping("")
     @PreAuthorize("hasAuthority('USER')")
     fun update(@RequestBody comment: CommentUpdateDTO): ResponseEntity<String> {
         return try {
