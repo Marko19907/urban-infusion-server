22023255e0366e3fe90866fc471b1c8dc43dd9b1
Marko19907 <marko.stancic23@gmail.com>
2022-06-05 20:59:16 +0200

    Corrected the permissions of the /order endpoint

--- src/main/java/no/ntnu/webdev/webproject7/controllers/OrderController.kt
+++ src/main/java/no/ntnu/webdev/webproject7/controllers/OrderController.kt
@@ -40,6 +40,7 @@ class OrderController(
     }
 
     @PostMapping("")
+    @PreAuthorize("hasAuthority('USER')")
     fun addOne(@RequestBody entity: OrderDTO): ResponseEntity<String> {
         val user: User = this.userService.getSessionUser() ?: return ResponseEntity("Invalid login", HttpStatus.BAD_REQUEST);
         return if (this.orderService.add(entity, user)) ResponseEntity(HttpStatus.OK) else ResponseEntity(HttpStatus.BAD_REQUEST);
